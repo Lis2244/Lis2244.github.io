c259242c5d704fabec168fa93eeba0c1bc14a0d9
Lis2244 <103378132+Lis2244@users.noreply.github.com>
2023-12-19 16:26:21 +0300

    Почти доделал

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,9 @@ const map = L.map('map')
     console.log('Карта инициализирована')
   })
   .setView({
-    lat: 45.07523,
-    lng: 38.90142,
-  }, 11);
+    lat: 55.8522,
+    lng: 37.5506,
+  }, 6);
 
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -15,16 +15,22 @@ const map = L.map('map')
   ).addTo(map);
 
   const redPinIcon = L.icon({
-    iconUrl: 'images/red_icon.png',
+    iconUrl: 'images/red_metka.svg',
+    iconSize: [40, 52],
+    iconAnchor: [20, 52],
+  });
+
+  const bluePinIcon = L.icon({
+    iconUrl: 'images/blue_metka.svg',
     iconSize: [40, 52],
     iconAnchor: [20, 52],
   });
 
   const marker1 = L.marker(
     {
-        title: 'Женя зарыл хабар',
-        lat: 45.07627,
-        lng: 38.90765,
+        title: 'Рыбхоз Гжелка',
+        lat: 55.55582,
+        lng: 38.32232,
     },
     {
       icon: redPinIcon,
@@ -32,9 +38,9 @@ const map = L.map('map')
   );
   const marker2 = L.marker(
     {
-        title: 'Здесь Влад учил Женю грамоте',
-        lat: 45.03726,
-        lng: 38.95262,
+        title: 'Платная Рыбалка',
+        lat: 55.97102,
+        lng: 35.92565,
     },
     {
       icon: redPinIcon,
@@ -42,9 +48,9 @@ const map = L.map('map')
   );
   const marker3 = L.marker(
     {
-        title: 'Влад поймал сома маленького',
-        lat: 45.02832,
-        lng: 38.99170,
+        title: 'Рыбхоз Гжелка',
+        lat: 55.55540,
+        lng: 38.31686,
     },
     {
       icon: redPinIcon,
@@ -52,19 +58,19 @@ const map = L.map('map')
   );
   const marker4 = L.marker(
     {
-        title: 'Здесь любит рыбачить Саня',
-        lat: 45.00699,
-        lng: 38.99674,
+        title: 'Коняшинский карьер',
+        lat: 55.62697,
+        lng: 38.44509,
     },
     {
-      icon: redPinIcon,
+      icon: bluePinIcon,
     },
   );
   
-  marker1.bindPopup("<h3>Женя зарыл хабар</h3><img src='/images/photo_5192669423289354643_y.jpg' alt='Картинка' width='100' height='120'/>").addTo(map);
-  marker2.bindPopup("<h3>Здесь Влад учил Женю грамоте</h3><img src='/images/draka.jpg' alt='Картинка' width='160' height='120'/>").addTo(map);
-  marker3.bindPopup("<h3>Влад поймал сома маленького</h3><img src='/images/som.jpeg' alt='Картинка' width='100' height='120'/>").addTo(map);
-  marker4.bindPopup("<h3>Здесь любит рыбачить Саня</h3><img src='/images/fishing.jpg' alt='Картинка' width='170' height='120'/>").addTo(map);
+  marker1.bindPopup("<h3>Рыбхоз Гжелка</h3><img src='/images/gzhelka/gzhelka.jpg' alt='Садок с карпом' width='230' height='300'/><p>Вес рыбы в среднем 1,2кг. <a href='https://gzhelka.club/' target='_blank'>Подробнее</a></p>").addTo(map);
+  marker2.bindPopup("<h3>Платная Рыбалка</h3><img src='/images/pagubino/fishing_pagubino.jpg' alt='Садок с рыбой' width='230' height='300'/><p>Вес рыбы в среднем 1кг. <a href='https://yandex.ru/maps/org/platnaya_rybalka/65955710105/?ll=35.924734%2C55.970904&z=17' target='_blank'>Подробнее</a></p>").addTo(map);
+  marker3.bindPopup("<h3>Рыбхоз Гжелка</h3><img src='/images/gzhelka/schuka_min.jpg' alt='Щука' width='230' height='300'/><p>Вес рыбы в среднем 0,7кг. <a href='https://gzhelka.club/' target='_blank'>Подробнее</a></p>").addTo(map);
+  marker4.bindPopup("<h3>Коняшинский карьер</h3><img src='/images/konyashino/meloch.jpeg' alt='Маленькие рыбки' width='230' height='300'/><p>Вес рыбы в среднем 0,2кг. Мне просто не повезло, говорят тут гиганты водятся</p>").addTo(map);
 
 
 
