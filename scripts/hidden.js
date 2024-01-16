19c1e1e5b09db74c01c02289bc075452e68f4170
Lis2244 <103378132+Lis2244@users.noreply.github.com>
2024-01-16 22:34:31 +0300

    Сайт 3д визуализации

--- scripts/hidden.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function(){
-    document.querySelector('.navigation_link_text').classList.add('color_text_js');
-}, 400);
-
-setTimeout(function(){
-    document.querySelector('.telegram_svg').classList.add('telegram_svg_position');
-}, 200);
