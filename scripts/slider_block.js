be9bfb1cf94b01e9f837be384bbb1cd12dbc8ff9
Lis2244 <103378132+Lis2244@users.noreply.github.com>
2024-05-26 15:00:01 +0300

    kommit

--- scripts/slider_block.js
+++ scripts/slider_block.js
@@ -1,86 +1,77 @@
-let namberOne = document.querySelector('.namber_one');
-let namberTwo = document.querySelector('.namber_two');
-let namberThree = document.querySelector('.namber_three');
-let namberFor = document.querySelector('.namber_for');
-let namberFife = document.querySelector('.namber_fife');
+let namberOne = document.querySelector('.text_one');
+let namberTwo = document.querySelector('.text_two');
+let namberThree = document.querySelector('.text_three');
+let namberFor = document.querySelector('.text_for');
+let namberFife = document.querySelector('.text_fife');
+let namberSix = document.querySelector('.text_six');
+let namberSevan = document.querySelector('.text_sevan');
 
-let textheaderOne = document.querySelector('.london_text_one');
-let textheaderTwo = document.querySelector('.london_text_two');
-let textheaderThree = document.querySelector('.london_text_three');
-let textheaderFor = document.querySelector('.london_text_for');
-let textheaderFife = document.querySelector('.london_text_fife');
-
-let textheaderitemOne = document.querySelector('.one_name');
-let textheaderitemTwo = document.querySelector('.two_name');
-let textheaderitemThree = document.querySelector('.three_name');
-let textheaderitemFor = document.querySelector('.for_name');
-let textheaderitemFife = document.querySelector('.fife_name');
-
-let textheaderbox = document.querySelector('.projects_item_header_one');
-let textheaderboxTwo = document.querySelector('.projects_item_header_two');
-let textheaderboxThree = document.querySelector('.projects_item_header_three');
-let textheaderboxFor = document.querySelector('.projects_item_header_for');
-let textheaderboxFife = document.querySelector('.projects_item_header_fife');
 
 let progectOne = document.querySelector('.gallary_box_one');
 let progectTwo = document.querySelector('.gallary_box_two');
 let progectThree = document.querySelector('.gallary_box_three');
 let progectFor = document.querySelector('.gallary_box_for');
 let progectFife = document.querySelector('.gallary_box_fife');
+let progectSix = document.querySelector('.gallary_box_six');
+let progectSevan = document.querySelector('.gallary_box_sevan');
+
 
 let buttonOne = document.querySelector('.button_plus_one');
 let buttonTwo = document.querySelector('.button_plus_two');
 let buttonThree = document.querySelector('.button_plus_three');
 let buttonFor = document.querySelector('.button_plus_for');
 let buttonFife = document.querySelector('.button_plus_fife');
+let buttonSix = document.querySelector('.button_plus_six');
+let buttonSevan = document.querySelector('.button_plus_sevan');
 
-let transformPlus = document.querySelector('.plus_one');
+
+let transformPlusOne = document.querySelector('.plus_one');
 let transformPlusTwo = document.querySelector('.plus_two');
 let transformPlusThree = document.querySelector('.plus_three');
 let transformPlusFor = document.querySelector('.plus_for');
 let transformPlusFife = document.querySelector('.plus_fife');
+let transformPlusSix = document.querySelector('.plus_six');
+let transformPlusSevan = document.querySelector('.plus_sevan');
+
 
 buttonOne.onclick = function () {
-    namberOne.classList.toggle('item_namber_hiden');
-    textheaderOne.classList.toggle('london_text_visability');
-    textheaderitemOne.classList.toggle('projects_item_name_visability');
-    textheaderbox.classList.toggle('projects_item_header_visability');
+    namberOne.classList.toggle('item_heding_hiden');
     progectOne.classList.toggle('display_visability');
-    transformPlus.classList.toggle('plus_transform');
+    transformPlusOne.classList.toggle('plus_transform');
 }
 
 buttonTwo.onclick = function () {
-    namberTwo.classList.toggle('item_namber_hiden');
-    textheaderTwo.classList.toggle('london_text_visability');
-    textheaderitemTwo.classList.toggle('projects_item_name_visability');
-    textheaderboxTwo.classList.toggle('projects_item_header_visability');
+    namberTwo.classList.toggle('item_heding_hiden');
     progectTwo.classList.toggle('display_visability');
     transformPlusTwo.classList.toggle('plus_transform');
 }
 
 buttonThree.onclick = function () {
-    namberThree.classList.toggle('item_namber_hiden');
-    textheaderThree.classList.toggle('london_text_visability');
-    textheaderitemThree.classList.toggle('projects_item_name_visability');
-    textheaderboxThree.classList.toggle('projects_item_header_visability');
+    namberThree.classList.toggle('item_heding_hiden');
     progectThree.classList.toggle('display_visability');
     transformPlusThree.classList.toggle('plus_transform');
 }
 
 buttonFor.onclick = function () {
-    namberFor.classList.toggle('item_namber_hiden');
-    textheaderFor.classList.toggle('london_text_visability');
-    textheaderitemFor.classList.toggle('projects_item_name_visability');
-    textheaderboxFor.classList.toggle('projects_item_header_visability');
+    namberFor.classList.toggle('item_heding_hiden');
     progectFor.classList.toggle('display_visability');
     transformPlusFor.classList.toggle('plus_transform');
 }
 
 buttonFife.onclick = function () {
-    namberFife.classList.toggle('item_namber_hiden');
-    textheaderFife.classList.toggle('london_text_visability');
-    textheaderitemFife.classList.toggle('projects_item_name_visability');
-    textheaderboxFife.classList.toggle('projects_item_header_visability');
+    namberFife.classList.toggle('item_heding_hiden');
     progectFife.classList.toggle('display_visability');
     transformPlusFife.classList.toggle('plus_transform');
+}
+
+buttonSix.onclick = function () {
+    namberSix.classList.toggle('item_heding_hiden');
+    progectSix.classList.toggle('display_visability');
+    transformPlusSix.classList.toggle('plus_transform');
+}
+
+buttonSevan.onclick = function () {
+    namberSevan.classList.toggle('item_heding_hiden');
+    progectSevan.classList.toggle('display_visability');
+    transformPlusSevan.classList.toggle('plus_transform');
 }
